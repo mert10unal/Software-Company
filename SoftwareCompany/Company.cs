0a38e6a0b59a0e72a787e76df4af2bdb57af3bf7
mert10unal <unalmert883@gmail.com>
2023-02-15 14:33:20 +0300

    software company database

---
+++ SoftwareCompany/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SoftwareCompany
+{
+	public class Company
+	{
+		public int Id { get; set; }
+		public string CompanyName { get; set; }
+		public int TaxNumber { get; set; }
+		public string Address { get; set; }
+		public int CompanyDetailId { get; set; }
+
+        public List<Worker> Workers { get; set; }
+        public List<Contract> Contracts { get; set; }
+		public CompanyDetail CompanyDetail { get; set; }
+    }
+}
+
---
+++ SoftwareCompany/CompanyDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SoftwareCompany
+{
+	public class CompanyDetail
+	{
+		public int Id { get; set; }
+        public int CompanyId { get; set; }
+		public int TaxNumber { get; set; }
+
+
+        public Company Company { get; set; }
+    }
+}
+
